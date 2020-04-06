781db1e04e0a303ef12e16c3047351c1a7331f31
valeriiakuzina <valeriiakuzina@gmail.com>
2020-04-06 19:47:27 +0600

    first version hw

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String args[]) {
+        // TODO: в переменную ticket вписать цену за билет
+        double ticket = 10039.80;
+        int mile = (int) (ticket/20);
+        System.out.println("Начислено миль " + mile);
+    }
+}
